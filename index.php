4e08e046a4faab8b752bbfd218bd8cb762c0915f
FTeguh <Fteguh97@gmail.com>
2022-04-05 11:32:59 +0700

    brewayy

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Web Statis</title>
+</head>
+<body>
+  <nav>
+    <div class="container">
+      <a href="">Home</a>
+      <a href="">Contact</a>
+      <a href="">About</a>
+    </div>
+  </nav>
+  <div class="container">
+    <h2>BREAKING NEW'S WHY</h2>
+    <h1>Pria ini Ciptakan Rok Ayam Untuk Memikat Si Kaum Adam </h1>
+    <img class="img" src="ayam.png">
+    <p>Upluoad by tgh 20/02/2022</p>
+    <hr>
+    <p class="aut"> "Seorang pria diketahui bernama dustin menciptakan atau membuat rok ayam tapi pria ini punya alasan tersendiri. ia sudah lama meneliti tentang tarian dan ia percaya bahwa rok ayam ini bisa untuk memikat si kaum adam"</p>
+  </div>
+</body>
+</html>
